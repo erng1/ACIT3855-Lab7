6d8d88bda03f6e8d652d88003491d89400c94186
Eric Ng <ericng_7@hotmail.com>
2021-11-14 22:39:42 -0800

    Added dashboard-ui

---
+++ dashboard-ui/src/components/AppStats.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css';
+
+export default function AppStats() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [stats, setStats] = useState({});
+    const [error, setError] = useState(null)
+
+	const getStats = () => {
+	
+        fetch(`http://kafka-acit3855-lab6a.eastus2.cloudapp.azure.com:8100/stats`)
+            .then(res => res.json())
+            .then((result)=>{
+				console.log("Received Stats")
+                setStats(result);
+                setIsLoaded(true);
+            },(error) =>{
+                setError(error)
+                setIsLoaded(true);
+            })
+    }
+    useEffect(() => {
+		const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
+		return() => clearInterval(interval);
+    }, [getStats]);
+
+    if (error){
+        return (<div className={"error"}>Error found when fetching from API</div>)
+    } else if (isLoaded === false){
+        return(<div>Loading...</div>)
+    } else if (isLoaded === true){
+        return(
+            <div>
+                <h1>Latest Stats</h1>
+                <table className={"StatsTable"}>
+					<tbody>
+						<tr>
+							<th>Weather Forecast</th>
+							<th>Misc Weather</th>
+						</tr>
+						<tr>
+							<td># WF: {stats['num_wf_reports']}</td>
+							<td># MW: {stats['num_mw_reports']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">Max Temperature: {stats['max_temperature']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">Min Temperature: {stats['min_temperature']}</td>
+						</tr>
+					</tbody>
+                </table>
+                <h3>Last Updated: {stats['last_updated']}</h3>
+
+            </div>
+        )
+    }
+}
